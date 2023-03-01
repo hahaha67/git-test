4d3fc450b62553d77d27d7cf438eddee8637662e
Msv <Msv.com>
2023-03-01 14:02:36 +0800

    second commit

--- src/main/java/com/guigu/Main.java
+++ src/main/java/com/guigu/Main.java
@@ -3,5 +3,6 @@ package com.guigu;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+        System.out.println("Hello git222!");
     }
 }
